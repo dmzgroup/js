3e8a2f59225dbf35cf4ad8ecce05af96d4d647e3
Randall E. Barker <simstorm@mac.com>
2010-10-12 12:05:51 -0700

    Added start of file ext.

---
+++ extensions/v8/dmzJsExtV8File.cpp
@@ -0,0 +1,206 @@
+#include "dmzJsExtV8File.h"
+#include <dmzJsModuleV8.h>
+#include <dmzJsV8UtilConvert.h>
+#include <dmzRuntimePluginFactoryLinkSymbol.h>
+#include <dmzRuntimePluginInfo.h>
+#include <dmzSystemFile.h>
+
+dmz::JsExtV8File::JsExtV8File (const PluginInfo &Info, Config &local) :
+      Plugin (Info),
+      JsExtV8 (Info),
+      _log (Info),
+      _core (0) {
+
+   _init (local);
+}
+
+
+dmz::JsExtV8File::~JsExtV8File () {
+
+}
+
+
+// Plugin Interface
+void
+dmz::JsExtV8File::update_plugin_state (
+      const PluginStateEnum State,
+      const UInt32 Level) {
+
+   if (State == PluginStateInit) {
+
+   }
+   else if (State == PluginStateStart) {
+
+   }
+   else if (State == PluginStateStop) {
+
+   }
+   else if (State == PluginStateShutdown) {
+
+   }
+}
+
+
+void
+dmz::JsExtV8File::discover_plugin (
+      const PluginDiscoverEnum Mode,
+      const Plugin *PluginPtr) {
+
+   if (Mode == PluginDiscoverAdd) {
+
+   }
+   else if (Mode == PluginDiscoverRemove) {
+
+   }
+}
+
+
+// JsExtV8 Interface
+void
+dmz::JsExtV8File::update_js_module_v8 (const ModeEnum Mode, JsModuleV8 &module) {
+
+   if (Mode == JsExtV8::Store) { if (!_core) { _core = &module; } }
+   else if (Mode == JsExtV8::Remove) { if (&module == _core) { _core = 0; } }
+}
+
+
+void
+dmz::JsExtV8File::update_js_context_v8 (v8::Handle<v8::Context> context) {
+
+   _v8Context = context;
+}
+
+
+void
+dmz::JsExtV8File::update_js_ext_v8_state (const StateEnum State) {
+
+   v8::HandleScope scope;
+
+   if (State == JsExtV8::Register) {
+
+      if (_core) {
+
+         _core->register_interface (
+            "dmz/system/file",
+            _fileApi.get_new_instance ());
+      }
+   }
+   else if (State == JsExtV8::Init) {
+
+   }
+   else if (State == JsExtV8::Stop) {
+
+   }
+   else if (State == JsExtV8::Shutdown) {
+
+      _fileApi.clear ();
+      _v8Context.Clear ();
+   }
+}
+
+
+void
+dmz::JsExtV8File::release_js_instance_v8 (
+      const Handle InstanceHandle,
+      const String &InstanceName,
+      v8::Handle<v8::Object> &instance) {;}
+
+
+// JsExtV8File Interface
+dmz::V8Value
+dmz::JsExtV8File::_file_split (const v8::Arguments &Args) {
+
+   v8::HandleScope scope;
+
+   V8Value result = v8::Undefined ();
+
+   if (Args.Length () > 0) {
+
+      if (Args[0]->IsString ()) {
+
+         const String FilePath = v8_to_string (Args[0]);
+
+         String path, file, ext;
+         split_path_file_ext (FilePath, path, file, ext);
+
+         V8Object obj = v8::Object::New ();
+
+         if (obj.IsEmpty () == false) {
+
+            if (path) {
+
+               obj->Set (
+                  v8::String::NewSymbol("path"),
+                  v8::String::New (path.get_buffer ()));
+            }
+
+            if (file) {
+
+               obj->Set (
+                  v8::String::NewSymbol("file"),
+                  v8::String::New (file.get_buffer ()));
+            }
+
+            if (ext) {
+
+               obj->Set (
+                  v8::String::NewSymbol("ext"),
+                  v8::String::New (ext.get_buffer ()));
+            }
+
+            result = obj;
+         }
+      }
+   }
+
+   return scope.Close (result);
+}
+
+
+dmz::V8Value
+dmz::JsExtV8File::_file_valid (const v8::Arguments &Args) {
+
+   v8::HandleScope scope;
+
+   V8Value result = v8::Undefined ();
+
+   if (Args.Length () > 0) {
+
+      if (Args[0]->IsString ()) {
+
+         const String FilePath = v8_to_string (Args[0]);
+
+         result = v8::Boolean::New (is_valid_path (v8_to_string (Args[0])));
+      }
+   }
+
+   return scope.Close (result);
+}
+
+
+void
+dmz::JsExtV8File::_init (Config &local) {
+
+   v8::HandleScope scope;
+
+   _self = V8ValuePersist::New (v8::External::Wrap (this));
+
+   // Bind API
+   _fileApi.add_function ("split", _file_split, _self);
+   _fileApi.add_function ("valid", _file_valid, _self);
+
+}
+
+
+extern "C" {
+
+DMZ_PLUGIN_FACTORY_LINK_SYMBOL dmz::Plugin *
+create_dmzJsExtV8File (
+      const dmz::PluginInfo &Info,
+      dmz::Config &local,
+      dmz::Config &global) {
+
+   return new dmz::JsExtV8File (Info, local);
+}
+
+};
---
+++ extensions/v8/dmzJsExtV8File.h
@@ -0,0 +1,70 @@
+#ifndef DMZ_JS_EXT_V8_FILE_DOT_H
+#define DMZ_JS_EXT_V8_FILE_DOT_H
+
+#include <dmzJsExtV8.h>
+#include <dmzJsV8UtilHelpers.h>
+#include <dmzRuntimeLog.h>
+#include <dmzRuntimePlugin.h>
+
+namespace dmz {
+
+   class JsExtV8File :
+         public Plugin,
+         public JsExtV8 {
+
+      public:
+         JsExtV8File (const PluginInfo &Info, Config &local);
+         ~JsExtV8File ();
+
+         // Plugin Interface
+         virtual void update_plugin_state (
+            const PluginStateEnum State,
+            const UInt32 Level);
+
+         virtual void discover_plugin (
+            const PluginDiscoverEnum Mode,
+            const Plugin *PluginPtr);
+
+         // JsExtV8 Interface
+         virtual void update_js_module_v8 (const ModeEnum Mode, JsModuleV8 &module);
+         virtual void update_js_context_v8 (v8::Handle<v8::Context> context);
+         virtual void update_js_ext_v8_state (const StateEnum State);
+
+         virtual void release_js_instance_v8 (
+            const Handle InstanceHandle,
+            const String &InstanceName,
+            v8::Handle<v8::Object> &instance);
+
+      protected:
+         // JsExtV8File Interface
+         static JsExtV8File *_to_self (const v8::Arguments &Args);
+         static V8Value _file_split (const v8::Arguments &Args);
+         static V8Value _file_valid (const v8::Arguments &Args);
+         void _init (Config &local);
+
+         Log _log;
+
+         JsModuleV8 *_core;
+
+         V8InterfaceHelper _fileApi;
+
+         v8::Handle<v8::Context> _v8Context;
+
+         V8ValuePersist _self;
+
+      private:
+         JsExtV8File ();
+         JsExtV8File (const JsExtV8File &);
+         JsExtV8File &operator= (const JsExtV8File &);
+
+   };
+};
+
+
+inline dmz::JsExtV8File *
+dmz::JsExtV8File::_to_self (const v8::Arguments &Args) {
+
+   return (dmz::JsExtV8File *)v8::External::Unwrap (Args.Data ());
+}
+
+#endif // DMZ_JS_EXT_V8_FILE_DOT_H
